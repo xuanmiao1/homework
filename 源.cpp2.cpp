6cb9f7782de4d546a3c0a163139bf1bd9b70e007
xuanmiao1 <55395539+xuanmiao1@users.noreply.github.com>
2019-09-22 17:53:08 +0800

    Add files via upload

---
+++ 源.cpp2.cpp
@@ -0,0 +1,10 @@
+using namespac std;
+int main()
+{
+	foloat a, b, c;
+	float p, s;
+	cout《“请输入三角形的三边长：”《endcin》a》b》c；
+
+}cout《“三角形的三条边长：”《a《\""《b《“”《c《endl;
+p = (a + b + c) / ;
+S = sqrt(p*(p - a)*（p - b)*(p - c));
